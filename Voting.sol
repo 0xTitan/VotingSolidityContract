2931807fb397c76df291da477ec0d6db121294f2
0xTitan <clement.suzat@gmail.com>
2022-06-07 15:10:23 +0200

    voter and proposal function

--- Voting.sol
+++ Voting.sol
@@ -25,12 +25,51 @@ contract Voting is Ownable {
         VotingSessionStarted,
         VotingSessionEnded,
         VotesTallied
-}
+    }
+
+    //events
+    event VoterRegistered(address voterAddress); 
+    event WorkflowStatusChange(WorkflowStatus previousStatus, WorkflowStatus newStatus);
+    event ProposalRegistered(uint proposalId);
+    event Voted (address voter, uint proposalId);
+    //current workflow
+    WorkflowStatus currentWorkflow;
 
     //list addess allowed to vote (whitelist)
-    mapping(address => bool) whitelistedAddresses;
-    //array representing proposal list
-    Proposal[] public proposals;
+    mapping(address => Voter) public whitelistedAddresses;
+    //proposal list
+    mapping(uint => Proposal) public proposals;
+    //incremental number to assign a unique id to a proposal
+    uint proposalId =0;
+
+
+    //register voters
+    function registerVoter(address _address) public onlyOwner {
+        require(uint8(currentWorkflow) ==0,"Phase invalid - Voter registration is forbidden");
+        require(_address !=address(0),"address invalid");
+        require(_address !=msg.sender,"Admin cannot participate");
+        require(!whitelistedAddresses[_address].isRegistered,"Address already registered");
+        //create voter with init value
+        Voter memory voter = Voter({isRegistered : true, hasVoted :false,votedProposalId : 0});
+        whitelistedAddresses[_address] = voter;
+        //send event
+        emit VoterRegistered(_address);
+    }
+
+     //register voters
+    function registerProposal(string memory _description) public onlyOwner {
+        require(uint8(currentWorkflow) ==1,"Phase invalid - Proposal registration is forbidden");
+        require(msg.sender !=address(0),"address invalid");
+        require(msg.sender !=owner(),"Admin cannot participate");
+        require(whitelistedAddresses[msg.sender].isRegistered,"Address not registered (whitelisted");
+        //create voter with init value
+        Proposal memory proposal = Proposal({description : _description, voteCount:0});
+        proposals[proposalId] = proposal;
+        //send event
+        emit ProposalRegistered(proposalId);
+        proposalId++;
+    }
+
 
     //Get the winning proposal id when votes are closed
     function winningProposalId() public view returns (uint){
@@ -41,4 +80,45 @@ contract Voting is Ownable {
     function getWinner () public view returns (Proposal memory){
 
     }
+
+
+    /******************* Workflow management********************/
+    function startRegisterVoterPhase() public onlyOwner{
+        require(uint8(currentWorkflow) <=0,"Phase invalid");
+        WorkflowStatus oldStatus = currentWorkflow;
+        currentWorkflow = WorkflowStatus.RegisteringVoters;
+        emit WorkflowStatusChange(oldStatus, currentWorkflow);
+    }
+
+    
+    function startProposalsRegistrationPhase() public onlyOwner{
+        require(uint8(currentWorkflow) ==0,"Phase invalid");
+        WorkflowStatus oldStatus = currentWorkflow;
+        currentWorkflow = WorkflowStatus.ProposalsRegistrationStarted;
+        emit WorkflowStatusChange(oldStatus, currentWorkflow);
+    }
+
+    
+    function endProposalsRegistrationPhase() public onlyOwner{
+        require(uint8(currentWorkflow) ==1,"Phase invalid");
+        WorkflowStatus oldStatus = currentWorkflow;
+        currentWorkflow = WorkflowStatus.ProposalsRegistrationEnded;
+        emit WorkflowStatusChange(oldStatus, currentWorkflow);
+    }
+
+   
+    function startVotingSessionPhase() public onlyOwner{
+        require(uint8(currentWorkflow) ==2,"Phase invalid");
+        WorkflowStatus oldStatus = currentWorkflow;
+        currentWorkflow = WorkflowStatus.VotingSessionStarted;
+        emit WorkflowStatusChange(oldStatus, currentWorkflow);
+    }
+
+    function endVotingSessionPhase() public onlyOwner{
+        require(uint8(currentWorkflow) ==3,"Phase invalid");
+        WorkflowStatus oldStatus = currentWorkflow;
+        currentWorkflow = WorkflowStatus.VotingSessionEnded;
+        emit WorkflowStatusChange(oldStatus, currentWorkflow);
+    }
+     /*************************************************************/
 }
