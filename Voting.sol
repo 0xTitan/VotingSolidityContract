062d715d0a46a7a13f5b698bd535a5e0f64359ea
0xTitan <clement.suzat@gmail.com>
2022-06-07 13:52:06 +0200

    add import

---
+++ Voting.sol
@@ -0,0 +1,9 @@
+// SPDX-License-Identifier: GPL-3.0
+pragma solidity >=0.8.0 <0.9.0;
+
+import "https://github.com/OpenZeppelin/openzeppelin-contracts/blob/a7818b49a481e867230d4fa49d106bd304c5d95d/contracts/access/Ownable.sol";
+
+contract Voting is Ownable {
+
+}
+
