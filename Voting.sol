d99042a21a4004794fa7fc23cb035327923084c2
0xTitan <clement.suzat@gmail.com>
2022-06-07 14:06:49 +0200

    add struct

--- Voting.sol
+++ Voting.sol
@@ -4,6 +4,41 @@ pragma solidity >=0.8.0 <0.9.0;
 import "https://github.com/OpenZeppelin/openzeppelin-contracts/blob/a7818b49a481e867230d4fa49d106bd304c5d95d/contracts/access/Ownable.sol";
 
 contract Voting is Ownable {
+    //voter struct
+    struct Voter {
+        bool isRegistered;
+        bool hasVoted;
+        uint256 votedProposalId;
+    }
 
+    //proposal struct
+    struct Proposal {
+        string description;
+        uint256 voteCount;
+    }
+
+    //voting workflow
+    enum WorkflowStatus {
+        RegisteringVoters,
+        ProposalsRegistrationStarted,
+        ProposalsRegistrationEnded,
+        VotingSessionStarted,
+        VotingSessionEnded,
+        VotesTallied
 }
 
+    //list addess allowed to vote (whitelist)
+    mapping(address => bool) whitelistedAddresses;
+    //array representing proposal list
+    Proposal[] public proposals;
+
+    //Get the winning proposal id when votes are closed
+    function winningProposalId() public view returns (uint){
+
+    }
+
+     //Get the proposal winner when votes are closed
+    function getWinner () public view returns (Proposal memory){
+
+    }
+}
